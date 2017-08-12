a0ac2f401364ef1f3ae943fe7c89562bf75fa6d7
Blake Griffith <cowlicks@riseup.net>
2017-08-11 19:10:00 -0700

    whoops wrong test

--- tests/selenium/super_cookie_test.py
+++ tests/selenium/super_cookie_test.py
@@ -31,11 +31,21 @@ class SuperCookieTest(pbtest.PBSeleniumTest):
         return self.js(CHECK_SNITCH_MAP_JS)
 
     def runme(self):
-        self.load_url("https://rawgit.com/gunesacar/24d81a5c964cb563614162c264be32f0/raw/8fa10f97b87343dfb62ae9b98b753c73a995157e/frame_ls.html",  # noqa
-                      wait_on_site=1)
-        self.driver.refresh()
-        time.sleep(1)
-        self.assertTrue(self.detected_tracking_by("githack.com"))
+        self.load_url("https://cdn.rawgit.com/ghostwords/d3685dc39f7e67dddf1edf2614beb6fc/raw/a78cfd6c86d51a8d8ab1e214e4e49e2c025d4715/privacy_badger_async_bug_test_fixture.html")
+
+        # the above HTML page reloads itself furiously to trigger our bug
+        # we need to wait for it to finish reloading
+        self.wait_for_script("return window.DONE_RELOADING === true")
+
+        # the HTML page contains:
+
+        # an iframe from gistcdn.githack.com that writes to localStorage
+        self.assertTrue(self.detected_tracking_by("githack.com"),
+            msg="IFrame sets localStorage but was not flagged as a tracker.")
+
+        # and an image from raw.githubusercontent.com that doesn't do any tracking
+        self.assertFalse(self.detected_tracking_by("raw.githubusercontent.com"),
+            msg="Image is not a tracker but was flagged as one.")
 
 for n in range(50):
     setattr(SuperCookieTest, 'test' + str(n), func_maker(n))
